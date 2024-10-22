e73acd9ff1c62ac5a4442e1d4f3cc56a7400d670
luis.beleno <luis.beleno@imagemaker.com>
2024-10-21 19:24:50 -0500

    modificaciones para el entrenamiento de SUpervisado

--- apsuper/entrenamiento.py
+++ apsuper/entrenamiento.py
@@ -11,14 +11,15 @@ from sklearn.preprocessing import StandardScaler
 # Deshabilitar las optimizaciones de oneDNN para evitar advertencias de rendimiento
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 
-hidden_layer=1
-n_neurones_hidden_layer=32
+hidden_layer = 1
+n_neurones_hidden_layer = 32
+n_neurones_layer_initial = 64
 
 data = pd.read_csv(os.path.join('data','breast-cancer.csv'))
 print(data.head())
 
-X = data.drop(columns=['id', 'diagnosis'])  # Excluir las columnas 'id' y 'diagnosis'
-y = data['diagnosis'].apply(lambda x: 1 if x == 'M' else 0)  # Convertir 'M' en 1 y 'B' en 0
+X = data.drop(columns=['id', 'diagnosis'])
+y = data['diagnosis'].apply(lambda x: 1 if x == 'M' else 0)  
 
 # se dividen los datos para entrenamiento y para pruebas (70/30)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
@@ -28,13 +29,12 @@ X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
 model = Sequential()
-model.add(Dense(64, activation='relu', input_shape=(X_train_scaled.shape[1],))) # capa de entrada con 64 Neuronas
+model.add(Dense(n_neurones_layer_initial, activation='relu', input_shape=(X_train_scaled.shape[1],))) # capa de entrada con n Neuronas
 for layern in range(hidden_layer):
     name_layer = f"layer{layern+1}"
     model.add(Dense(n_neurones_hidden_layer, activation='relu', name=name_layer)) # capa oculta
 
-model.add(Dense(1, activation='sigmoid')) # Salida con funcion de activacion (salida binaria)3
-
+model.add(Dense(1, activation='sigmoid')) # Salida con funcion de activacion (salida binaria)
 
 # Compilar el modelo tipo adam
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
@@ -42,11 +42,10 @@ model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy']
 # Entrenamiento del modelo (40 épocas)
 history = model.fit(X_train_scaled, y_train, epochs=40, batch_size=32, validation_data=(X_test_scaled, y_test))
 
+# Se genera modelo respecto al numero de capas ocultas
 name_model_layers=f"mlp_model_super_{hidden_layer}_layers.h5"
 model.save(os.path.join('models',name_model_layers))
 
-
-# Evaluar el modelo en el conjunto de prueba
 loss, accuracy = model.evaluate(X_test_scaled, y_test)
 
 print(f"Precisión del modelo en el conjunto de prueba: {accuracy * 100:.2f}%")
